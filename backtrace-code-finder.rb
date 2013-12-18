3b3739d22c01de08a23c57b1c2fc1935848bc766
Nick Markwell <markwellna@upmc.edu>
2013-12-18 17:24:25 -0500

    Misc cleanup. (Wait, what? this is already three months old?! Holy shit!)

--- backtrace-code-finder.rb
+++ backtrace-code-finder.rb
@@ -1,23 +1,33 @@
 begin
   require caller.last.split(':').first
-rescue => e
+  exit
+rescue SystemExit
+  raise
+rescue Exception => e
   context = 3
 
   file, line = e.backtrace.first.split(':')
   line = line.to_i - 1
   code = File.open(file).read.split("\n")
 
-  puts "Code (~ indicates the line triggering the exception):"
+  puts
+  puts "~~~~~~~~~~~~~~~~"
+  puts
+  puts "The bolded code raised a#{['a', 'e', 'i', 'o', 'u'].include?(e.class.to_s[-1]) ? 'n' : ''} #{e.class}:"
   puts
 
-  ret = "    #{code[(line - context), context].join("\n    ")}\n" +
-        "~   #{code[line]}\n" +
-        "    #{code[(line + 1), context].join("\n    ")}"
+  ret = [
+          *code[(line - context), context],
+          "\e[1m#{code[line]}\e[22m",
+          *code[(line + 1), context]
+        ].join("\n")
 
   puts ret.rstrip # Avoids the problem of a bunch of blank lines showing up.
-
   puts
-  puts "#{e.class.name}: #{e.message}"
-  puts e.backtrace
+
+  puts "Backtrace:"
+  puts
+
+  raise
 end
 
--- test.rb
+++ test.rb
@@ -1,12 +1,15 @@
 #!/usr/bin/env ruby
-2
-3
+
 require './backtrace-code-finder.rb'
-5
-derp
-7
-8
-9
-10
-11
-12
+
+class Meep
+  def x
+    p 7
+    p 8
+    derp
+    p 10
+    p 11
+  end
+end
+
+Meep.new.x
