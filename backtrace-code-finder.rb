124bf6356b0b2012a0d1802340d719dee85f1064
Marie Markwell <me@marie.so>
2014-11-23 14:13:29 -0500

    Cleaned it up a ton.

--- backtrace-code-finder.rb
+++ backtrace-code-finder.rb
@@ -1,32 +1,45 @@
+class PrettyBacktrace
+  CONTEXT = 3
+
+  class << self
+    def print(exception)
+      file, line = exception.backtrace.first.split(':')
+      line = line.to_i - 1 # Zero indexed.
+
+      code = File.open(file).read.split("\n")
+
+      ret_ary = realign(code, line)
+      ret = ret_ary[(line - CONTEXT) .. (line + CONTEXT)].join("\n")
+
+      puts exception.backtrace.first
+      puts
+      puts ret.rstrip # Avoids the problem of a bunch of blank lines showing up.
+      puts
+
+      puts "Backtrace:"
+      puts
+    end
+
+    private
+    def realign(lines, error_line)
+      prefix_length = lines.length.to_s.length
+
+      lines.each_with_index.map do |line, i|
+        prefix = i == error_line ? '> ' : '  '
+
+        "#{prefix}#{line}"
+      end
+    end
+  end
+end
+
 begin
   require caller.last.split(':').first
   exit
 rescue SystemExit
   raise
 rescue Exception => e
-  context = 3
-
-  file, line = e.backtrace.first.split(':')
-  line = line.to_i - 1
-  code = File.open(file).read.split("\n")
-
-  puts
-  puts "~~~~~~~~~~~~~~~~"
-  puts
-  puts "The bolded code raised a#{['a', 'e', 'i', 'o', 'u'].include?(e.class.to_s[-1]) ? 'n' : ''} #{e.class}:"
-  puts
-
-  ret = [
-          *code[(line - context), context],
-          "\e[1m#{code[line]}\e[22m",
-          *code[(line + 1), context]
-        ].join("\n")
-
-  puts ret.rstrip # Avoids the problem of a bunch of blank lines showing up.
-  puts
-
-  puts "Backtrace:"
-  puts
+  PrettyBacktrace.print(e)
 
   raise
 end
